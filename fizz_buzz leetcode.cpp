bc2d8e45cf9e577cbef9f45e6b47c276b6c8618c
Bitani <Bethanyaloz755@gmail.com>
2022-04-19 04:22:35 -0600

    Create fizz_buzz leetcode.cpp

---
+++ fizz_buzz leetcode.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>answer(n,"");
+    for (int i = 0; i < answer.size(); ++i) 
+    {
+      if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0)
+      {
+        answer[i] = "FizzBuzz";
+      } 
+        else if ((i + 1) % 3 == 0)
+      {
+        answer[i] = "Fizz";
+      } 
+        else if ((i + 1) % 5 == 0) 
+      {
+        answer[i] = "Buzz";
+      } 
+        else 
+      {
+        answer[i] = to_string(i + 1);
+      }
+   }
+    
+    return answer;
+    }
+};
