57250fabb4dcb3728100296f31ca9b9b75c533da
Bitani <Bethanyaloz755@gmail.com>
2022-04-24 14:14:05 -0600

    Create Powx-n.cpp

---
+++ Powx-n.cpp
@@ -0,0 +1,10 @@
+// Time complexity of O(1).
+
+class Solution {
+public:
+    double myPow(double x, int n)
+    {
+      return pow(x, n);
+                 // return (double)pow(x, n);
+    }
+};
