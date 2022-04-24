d6cad3fa46a6cda3693e542bd4f62fbcd0a935d1
Bitani <Bethanyaloz755@gmail.com>
2022-04-24 14:15:33 -0600

    Update Powx-n.cpp

--- Powx-n.cpp
+++ Powx-n.cpp
@@ -2,6 +2,7 @@
 
 class Solution {
 public:
+    ////x is our number and n is the raised(power) number which means x raised to the power n.
     double myPow(double x, int n)
     {
       return pow(x, n);
