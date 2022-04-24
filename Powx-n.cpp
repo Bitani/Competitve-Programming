fe34f3407775100fa41f4d12ea11c7c5ac680884
Bitani <Bethanyaloz755@gmail.com>
2022-04-24 14:17:50 -0600

    Create Powx-n.cpp

---
+++ Powx-n.cpp
@@ -0,0 +1,11 @@
+// Time complexity of O(1).
+
+class Solution {
+public:
+    //x is our number and n is the raised(power) number which means x raised to the power n.
+    double myPow(double x, int n)
+    {
+      return pow(x, n);
+                 // return (double)pow(x, n);
+    }
+};
